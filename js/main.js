b3453e9de7232e452f64fea4104433431b430ca5
Ryan <RyanNeedham@outlook.com>
2021-12-11 10:07:15 +0000

    Update main.js
    changing default camera position to show the scene on reset, instead of going to the origin

--- js/main.js
+++ js/main.js
@@ -1061,8 +1061,8 @@
 
         if (event.key == 'r')
         {
-            CameraPosition = vec4(0.0, 0.0, 0.0, 0.0);
-            CameraRotation = new Float32Array([0.0, 0.0, 0.0]);
+            CameraPosition = vec4(0.0, 2.0, 9.0, 0.0);
+            CameraRotation = new Float32Array([0.1, 0.0, -1.0]);
         }
     }
 
